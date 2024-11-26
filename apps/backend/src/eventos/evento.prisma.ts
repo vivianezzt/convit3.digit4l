b95c7898cb261eb4289af7540cd0e75c2f084058
Viviane <vivianeaguiarc@outlook.com>
2024-11-25 21:20:03 -0300

    feat(database): implementação do serviço EventoPrisma
    - Criado o serviço `EventoPrisma` para integração com o banco de dados usando Prisma.
    - Adicionado método para salvar eventos no banco.
    - Implementado método para salvar convidados vinculados a eventos.
    - Adicionado método para buscar todos os eventos cadastrados.
    - Criados métodos para buscar eventos por ID ou alias, com suporte a inclusão de dados completos como convidados e senha.
    - Configurado o uso de `PrismaProvider` para gerenciamento das operações com o Prisma.

--- apps/backend/src/eventos/evento.prisma.ts
+++ apps/backend/src/eventos/evento.prisma.ts
@@ -1,7 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { Convidado, Evento } from 'core';
 import { PrismaProvider } from 'src/db/prisma.provider';
 
 @Injectable()
 export class EventoPrisma {
   constructor(readonly prisma: PrismaProvider) {}
+
+  salvar(evento: Evento) {
+    return this.prisma.evento.create({
+      data: evento as any,
+    });
+  }
+  salvarConvidado(evento: Evento, convidado: Convidado) {
+    return this.prisma.convidado.create({
+      data: {
+        ...convidado,
+        qtdeAcompanhantes: +(convidado.qtdeAcompanhantes || 0),
+        evento: { connect: { id: evento.id } },
+      },
+    });
+  }
+  async buscarTodos(): Promise<Evento[]> {
+    return this.prisma.evento.findMany() as any;
+  }
+  async buscarPorId(
+    id: string,
+    completo: boolean = false,
+  ): Promise<Evento | null> {
+    return this.prisma.evento.findUnique({
+      where: { id },
+      include: { convidados: completo },
+    }) as any;
+  }
+  async buscarPorAlias(
+    alias: string,
+    completo: boolean = false,
+  ): Promise<Evento | null> {
+    return this.prisma.evento.findUnique({
+      select: {
+        id: true,
+        alias: true,
+        nome: true,
+        descricao: true,
+        data: true,
+        local: true,
+        imagem: true,
+        imagemBackground: true,
+        senha: completo,
+        convidados: completo,
+        publicoEsperado: completo,
+      },
+      where: { alias },
+    }) as any;
+  }
 }
